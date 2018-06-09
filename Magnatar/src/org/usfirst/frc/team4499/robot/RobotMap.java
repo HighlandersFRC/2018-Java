79a007df76a237e53d07051fc571f943747ff6d2
DESKTOP-81TSV9P\Owner <theelleris13x@gmail.com>
2018-06-08 18:11:27 -0700

    Magnatar reprogram initial push :)

---
+++ Magnatar/src/org/usfirst/frc/team4499/robot/RobotMap.java
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team4499.robot;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+public static DoubleSolenoid Shifters =new DoubleSolenoid (0,6,7);
+public static DoubleSolenoid Intake =new DoubleSolenoid (0,0,1);
+}
