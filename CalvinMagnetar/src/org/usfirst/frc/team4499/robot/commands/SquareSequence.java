2701490bbe41a37a8c8104dafe5b14fb3bb26e3b
Calvin Blunk <haveufelt.thegloww@gmail.com>
2018-07-08 13:57:40 -0600

    Added wait commands to the square sequence

--- CalvinMagnetar/src/org/usfirst/frc/team4499/robot/commands/SquareSequence.java
+++ CalvinMagnetar/src/org/usfirst/frc/team4499/robot/commands/SquareSequence.java
@@ -11,12 +11,20 @@ public class SquareSequence extends CommandGroup {
 
     public SquareSequence() {
         addSequential(new MoveForward(RobotMap.leftDriveMaster, RobotMap.rightDriveMaster, 1));
+        addSequential(new Wait(0.5));
         addSequential(new NavXTurn(90));
+        addSequential(new Wait(0.5));
         addSequential(new MoveForward(RobotMap.leftDriveMaster, RobotMap.rightDriveMaster, 1));
+        addSequential(new Wait(0.5));
         addSequential(new NavXTurn(180));
+        addSequential(new Wait(0.5));
         addSequential(new MoveForward(RobotMap.leftDriveMaster, RobotMap.rightDriveMaster, 1));
+        addSequential(new Wait(0.5));
         addSequential(new NavXTurn(270));
+        addSequential(new Wait(0.5));
         addSequential(new MoveForward(RobotMap.leftDriveMaster, RobotMap.rightDriveMaster, 1));
+        addSequential(new Wait(0.5));
         addSequential(new NavXTurn(360));
+        addSequential(new Wait(0.5));
     }
 }
