c0193f4a950786424b56c7d508c66acb9536113e
Calvin Blunk <haveufelt.thegloww@gmail.com>
2018-09-07 18:35:20 -0600

    Fixed the Robot.java

--- CalvinMagnetar/src/org/usfirst/frc/team4499/robot/Robot.java
+++ CalvinMagnetar/src/org/usfirst/frc/team4499/robot/Robot.java
@@ -42,7 +42,6 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotInit() {
 		m_oi = new OI();
-		m_chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", m_chooser);
 		RobotMap.leftDriveFollower.set(ControlMode.Follower,  RobotMap.leftDriveMasterID);
